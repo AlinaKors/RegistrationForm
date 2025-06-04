85ed18e309d6daa21e22dad36906c51db8d9912f
Alina Korsukova <korsualin@gmail.com>
2025-06-04 20:04:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const regForm = document.getElementById('registrationForm');
+
+// проверка полей на валидацию
+const validateField = (input, tooltip) => {
+  if (!input.checkValidity()) {
+    input.classList.add('error');
+    tooltip.classList.add('show');
+    return false;
+  } else {
+    input.classList.remove('error');
+    tooltip.classList.remove('show');
+    return true;
+  }
+};
+
+// сбрасываем форму + выводим в консоли данные + отправляем данные
+const successRegistration = async (regForm) => {
+  const newUser = {
+    login: regForm.login.value,
+    password: regForm.password.value,
+    lastname: regForm.lastname.value,
+    firstname: regForm.firstname.value,
+    middlename: regForm.middlename.value,
+    city: regForm.city.value,
+  };
+  console.log('Данные регистрации:', newUser);
+  regForm.reset();
+  regForm.querySelectorAll('input').forEach((i) => i.classList.remove('error'));
+  regForm.querySelectorAll('.error-tooltip').forEach((t) => t.classList.remove('show'));
+
+  await fetch('https://483df46a630e924e.mokky.dev/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(newUser),
+  });
+};
+
+//отправка формы
+regForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let isValid = true;
+
+  const groups = regForm.querySelectorAll('.form-group');
+  groups.forEach((group) => {
+    const input = group.querySelector('input');
+    const tooltip = group.querySelector('.error-tooltip');
+    const valid = input.id !== 'middlename' && validateField(input, tooltip);
+    if (!valid) isValid = false;
+  });
+
+  isValid && successRegistration();
+});
+
+regForm.querySelectorAll('input').forEach((input) => {
+  const tooltip = input.parentElement.querySelector('.error-tooltip');
+  input.addEventListener('input', () => {
+    validateField(input, tooltip);
+  });
+});
